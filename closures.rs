f70f914f7548fe82cd611f293fdf71b634e89d6d
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-12 23:34:11 +0530

    added a cost expensive fn

--- closures.rs
+++ closures.rs
@@ -1,2 +1,9 @@
 use std::thread;
 use std::time duration 
+
+
+fn simulated_expensive_calculation(intensity:u32) -> u32{
+println!("calculating slowing...");
+thread::sleep(Duration::from_secs(2));
+intensity
+}
