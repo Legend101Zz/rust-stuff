476d8565bbc1fd4d49df0ace5e923efc8cd01ab5
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-17 22:17:46 +0530

    added closure fn and a memoization struct

--- closures.rs
+++ closures.rs
@@ -1,39 +1,42 @@
 use std::thread;
 use std::time::Duration;
 
-
-fn simulated_expensive_calculation(intensity:u32) -> u32{
-println!("calculating slowing...");
-thread::sleep(Duration::from_secs(2));
-intensity
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowing...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
 
+fn main() {
+    let simulated_intensity = 10;
+    let simulated_random_number = 7;
 
-fn main(){
-let simulated_intensity =10;
-let simulated_random_number = 7;
+    generate_workout(simulated_intensity, simulated_random_number);
+}
 
-generate_workout(simulated_intensity, simulated_random_number);
-  }
-
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
 
 //  ========= APPROACH WITHOUT USE OF CLOSURES =========
-// fn generate_workout(intensity:u32,random_number:u32){
-//     if intensity <25 {
-//       println!("Today, do {} pushups",simulated_expensive_calculation(intensity) ;
-//       println!("Next do , {} situps !",simulated_expensive_calculation(intensity) ;
-//     }
-
-//     else {
-//       if simulated_random_number == 3{
-
-//       println!("Take a break today , remember to stay hydrated") ;
-   
-//       }
-//       else{
-
-//       println!("Today, run for {} minutes",simulated_expensive_calculation(intensity) ;
-   
-//       }
-//     }
-// }
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println! { "Calculating slowing..." };
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    if intensity < 25 {
+        println!("Today, do {} pushups", expensive_closure(intensity));
+        println!("Next do , {} situps !", expensive_closure(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today , remember to stay hydrated");
+        } else {
+            println!("Today, run for {} minutes", expensive_closure(intensity));
+        }
+    }
+}
