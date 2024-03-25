1788ae6c84a11709a14020fc874b923187f7d3b2
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-25 22:49:30 +0530

    changes

--- iterators.rs
+++ iterators.rs
@@ -2,7 +2,14 @@ fn main() {
     let v1 = vec![1, 2, 3];
     let v1_iter = v1.iter();
 
+    let total: i32 = v1_iter.clone().sum();
+
     for value in v1_iter {
         println!("Got {} the value ", value)
     }
+
+    println!("Got {} the sum ", total);
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    println!(" {:?} ", v2)
 }
